5e151ad242d5e975a9e3446413e69511e4caa8f9
Nikita <enagtim@mail.ru>
2025-03-15 15:17:47 +0300

    Create config and connect database

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"api/email-verification/configs"
+	"api/email-verification/pkg/db"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	conf := configs.LoadConfig()
+	db := db.NewDb(conf)
+	router := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: router,
+	}
+	fmt.Println("Server start on port 8000")
+	server.ListenAndServe()
+	defer db.Close()
+}
--- go.mod
+++ go.mod
@@ -1,2 +1,8 @@
 module api/email-verification
-go 1.24.0 
+
+go 1.24.0
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
